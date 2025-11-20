6a813e18e8de970de298d6988dfd5ec243702dac
hagerradi <hager.radi@mila.quebec>
2025-11-20 10:58:06 -0500

    laoding elevation grid

---
+++ data_preparation/elevation_grid.py
@@ -0,0 +1,25 @@
+import os
+
+import numpy as np
+from utils import load_raster
+
+from data_preparation.visualize import visualize_elevation_grid
+
+ELEV_NATIONAL_MAX = 5855
+ELEV_NATIONAL_MIN = -158
+
+def load_elevation_grid(path: str)-> np.ma.MaskedArray:
+    """Load an FBP fuel raster and group fuel types if selected"""
+    elevation_grid = load_raster(path)
+    # normalize elevation grid data
+    elevation_grid = (elevation_grid - ELEV_NATIONAL_MIN) / (ELEV_NATIONAL_MAX - ELEV_NATIONAL_MIN)
+    return elevation_grid
+
+
+# TODO: delete later
+if __name__ == "__main__":
+    root_dir = "../yan_bp3/hex05"
+
+    out_elev_grid = load_elevation_grid(path=os.path.join(root_dir, "mapped_inputs/elev.asc"))  # noqa: F821
+    
+    visualize_elevation_grid(out_elev_grid)
