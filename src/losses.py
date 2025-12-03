512b7811ff845c9e2922a89870dc21f5ec879caf
hagerradi <hager.radi@mila.quebec>
2025-12-03 17:20:03 -0500

    add basic loss

--- src/losses.py
+++ src/losses.py
@@ -1,1 +1,23 @@
 # Defintions of loss functions
+import torch
+import torch.nn as nn
+
+
+class BCELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bce = nn.BCEWithLogitsLoss()
+
+    def forward(self, logits: torch.tensor, targets: torch.tensor):
+        # internally handles sigmoid
+        bce_loss = self.bce(logits, targets)
+
+        return bce_loss
+    
+# TODO: remove later
+if __name__ == "__main__":
+    preds = torch.randn(2, 1, 256, 256)
+    y = torch.ones(2, 1, 256, 256)
+    loss_fn = BCELoss()
+    output = loss_fn(preds, y)
+    print(f"Loss: {output.item()}")
