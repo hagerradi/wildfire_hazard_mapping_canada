4ca3193b7516479c5918618d4e685c917597f500
Chirag Goel <chirag.goel@mila.quebec>
2025-12-08 10:01:42 -0500

    Resolved comments

--- src/datasets/train_test_split.py
+++ src/datasets/train_test_split.py
@@ -26,22 +26,22 @@ def get_selected_hexel_metadata(data_dir:str, hex_ids:list)->pd.DataFrame:
     df =  pd.concat(df_list, ignore_index=True)
     return df
 
-def get_train_test_split(data_dir:str, val_hex_id:list, eval_hex_id:list):
+def get_data_splits(data_dir:str, val_hex_id:list, test_hex_id:list):
     """Split the data into train, val and test sets using the metadata csvs created"""
     #Get the train ids
     all_hex_ids = get_hex_ids(data_dir)
-    train_hex_ids = list(set(all_hex_ids) -  set(val_hex_id)- set(eval_hex_id))
+    train_hex_ids = list(set(all_hex_ids) -  set(val_hex_id)- set(test_hex_id))
 
     train_df = get_selected_hexel_metadata(data_dir, hex_ids=train_hex_ids)
     val_df = get_selected_hexel_metadata(data_dir, hex_ids=val_hex_id)
-    eval_df = get_selected_hexel_metadata(data_dir, hex_ids=eval_hex_id)
+    eval_df = get_selected_hexel_metadata(data_dir, hex_ids=test_hex_id)
 
     print("Total number of samples in train, val, eval are:",len(train_df), len(val_df), len(eval_df))
     
-    train_df.to_csv(os.path.join(data_dir, "train.csv"), index=False)
-    val_df.to_csv(os.path.join(data_dir, "val.csv"), index=False)
-    eval_df.to_csv(os.path.join(data_dir, "eval.csv"), index=False)
+    train_df.to_csv(os.path.join(data_dir, "train_indices.csv"), index=False)
+    val_df.to_csv(os.path.join(data_dir, "val_indices.csv"), index=False)
+    eval_df.to_csv(os.path.join(data_dir, "test_indices.csv"), index=False)
 
 if __name__ == "__main__":
     data_dir = "../yan_bp3/data_samples_approach_2"
-    get_train_test_split(data_dir=data_dir, val_hex_id=["16"], eval_hex_id=["41"])
+    get_data_splits(data_dir=data_dir, val_hex_id=["16"], test_hex_id=["41"])
