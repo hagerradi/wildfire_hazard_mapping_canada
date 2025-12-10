ceb5d4de12be9c425d6413021e47a08c34231eef
alzaia <aldo.zaimi+1@mila.quebec>
2025-12-10 10:27:46 -0500

    fix trainer precommit issues

--- src/config.py
+++ src/config.py
@@ -12,13 +12,14 @@ class OptimizerConfig(BaseModel):
     lr: float = 1e-3
     loss_name: str
 
+
 class TrainingConfig(BaseModel):
     max_epochs: int = 50
     log_every_n_epoch: int = 1
 
 
 class Config(BaseModel):
-    save_dir: str | None = "experiments/default"
+    save_dir: str = "experiments/default"
     model: ModelConfig
     optimizer: OptimizerConfig
     training: TrainingConfig
