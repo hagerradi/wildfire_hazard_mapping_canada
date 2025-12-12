c8949474db6997e1c9791bd0080a016f55381bbc
Chirag Goel <chirag.goel@mila.quebec>
2025-12-12 11:29:17 -0500

    Resolved comments

--- src/datasets/dataloader.py
+++ src/datasets/dataloader.py
@@ -7,12 +7,14 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader, Dataset
 
+from data_preparation.grid_loader.utils import fuel_ranking
+
 # Global Burn Count Min Max
 BURN_COUNT_MAX = 1336.0
 BURN_COUNT_MIN = 0.0
 
-MAX_FUEL_GRID = 20.0
-MIN_FUEL_GRID = 0.0
+MAX_FUEL_GRID = float(max(fuel_ranking.values()))
+MIN_FUEL_GRID = float(min(fuel_ranking.values()))
 
 
 def fill_nan_channel_mean_numpy(arr):
@@ -64,7 +66,7 @@ def one_hot_encode(arr, channel_idx, num_classes):
     # 4. Zero out the vectors where the original value was NaN
     # Before this, the NaNs were encoded as Class 0 (because we filled with 0)
     # This step corrects that by setting them to [0, 0, 0...]
-    encoded_part[nan_mask] = 0
+    encoded_part[nan_mask] = 0  # Nan is no fuel
 
     # 3. Concatenate along the channel axis (last axis)
     return np.concatenate([left_part, encoded_part.astype(float), right_part], axis=-1)
@@ -81,8 +83,8 @@ class GridDataset(Dataset):
         root_dir: str,
         filename_col: str = "filename",
         out_norm: str = "min_max",
-        fuel_feats_processing: str = "ordinal",
-        fuel_feats_ordinal_is_norm: bool | None = True,
+        fuel_feats_encoding: str = "ordinal",
+        normalize_fuel_feats_ordinal: bool | None = True,
         modelling_approach: str = "2",
         transform: Callable | None = None,
         feature_names_list: list[str] | None = None,
@@ -93,6 +95,8 @@ class GridDataset(Dataset):
             root_dir (str): Directory with all the .npy files.
             filename_col (str): Column name in CSV containing the filenames.
             out_norm (str): How to normalize the output burn counts. [Options: total_iters, season_cause_iters, min_max]
+            fuel_feats_encoding(str): How to process the fuel features [Options: ordinal, one_hot]
+            normalize_fuel_feats_ordinal (bool): If we want to normalize the ordinal encoded fuel feats
             modelling_approach (str): The approach used for modelling
             transform (callable, optional): Optional transform to be applied on a sample.
             feature_names_list (list): List of features being used for training ((options: None or feature list) All feats: ["ignition_grid", "esc_fires_grid", "fuel_grid", "elevation_grid", "weather_grid", "wind_grid"])
@@ -103,8 +107,8 @@ class GridDataset(Dataset):
         self.filename_col = filename_col
         self.transform = transform
         self.out_norm = out_norm
-        self.fuel_feats_processing = fuel_feats_processing
-        self.fuel_feats_ordinal_is_norm = fuel_feats_ordinal_is_norm
+        self.fuel_feats_encoding = fuel_feats_encoding
+        self.normalize_fuel_feats_ordinal = normalize_fuel_feats_ordinal
         if self.out_norm == "total_iters":
             self.out_norm_array = list(
                 self.metadata_df["total_unique_iters"]
@@ -122,7 +126,6 @@ class GridDataset(Dataset):
         self.fuel_feat_index = channel_feature_map["fuel_grid"][0]
         if feature_names_list:
             self.channel_indices = [item for key in feature_names_list for item in channel_feature_map[key]]
-            print(self.channel_indices)
             self.fuel_feat_index = self.channel_indices.index(self.fuel_feat_index)
 
     def __len__(self):
@@ -142,15 +145,16 @@ class GridDataset(Dataset):
         assert np.all(np.isnan(input_arr) == np.isnan(input_arr[..., :1])), "NaN mask differs across channels!"
         mask = np.isnan(input_arr[:, :, 0])  # return a mask for the loss function
         # Processing one hot encoding
-        if self.fuel_feats_processing == "one_hot":
+        if self.fuel_feats_encoding == "one_hot":  # (H,W,C+20)
             input_arr = one_hot_encode(arr=input_arr, channel_idx=self.fuel_feat_index, num_classes=int(MAX_FUEL_GRID + 1))
 
         input_arr = fill_nan_channel_mean_numpy(input_arr)  # remove NaNs from the inp data (replace by mean)
 
         # Processing ordinal encoding norm (if not norm do nothing)
-        if self.fuel_feats_processing == "ordinal":
-            input_arr[:, :, self.fuel_feat_index][mask] = 0.0
-            if self.fuel_feats_ordinal_is_norm:
+        if self.fuel_feats_encoding == "ordinal":
+            input_arr[:, :, self.fuel_feat_index][mask] = 0.0  # Nan is no fuel
+            if self.normalize_fuel_feats_ordinal:
+                print(MAX_FUEL_GRID, MIN_FUEL_GRID)
                 input_arr[:, :, self.fuel_feat_index] = (input_arr[:, :, self.fuel_feat_index] - MIN_FUEL_GRID) / (
                     MAX_FUEL_GRID - MIN_FUEL_GRID
                 )
@@ -175,8 +179,8 @@ def get_train_val_dataloader(
     batch_size: int = 4,
     num_workers: int = 0,
     out_norm: str = "min_max",
-    fuel_feats_processing: str = "ordinal",
-    fuel_feats_ordinal_is_norm: bool | None = True,
+    fuel_feats_encoding: str = "ordinal",
+    normalize_fuel_feats_ordinal: bool | None = True,
     modelling_approach: str = "2",
     transform: Callable | None = None,
     feature_names_list: list[str] | None = None,
@@ -189,8 +193,8 @@ def get_train_val_dataloader(
         root_dir=root_dir,
         filename_col=filename_col,
         out_norm=out_norm,
-        fuel_feats_processing=fuel_feats_processing,
-        fuel_feats_ordinal_is_norm=fuel_feats_ordinal_is_norm,
+        fuel_feats_encoding=fuel_feats_encoding,
+        normalize_fuel_feats_ordinal=normalize_fuel_feats_ordinal,
         modelling_approach=modelling_approach,
         transform=transform,
         feature_names_list=feature_names_list,
@@ -200,8 +204,8 @@ def get_train_val_dataloader(
         root_dir=root_dir,
         filename_col=filename_col,
         out_norm=out_norm,
-        fuel_feats_processing=fuel_feats_processing,
-        fuel_feats_ordinal_is_norm=fuel_feats_ordinal_is_norm,
+        fuel_feats_encoding=fuel_feats_encoding,
+        normalize_fuel_feats_ordinal=normalize_fuel_feats_ordinal,
         modelling_approach=modelling_approach,
         transform=transform,
         feature_names_list=feature_names_list,
@@ -221,8 +225,8 @@ def get_test_loader(
     batch_size: int = 4,
     num_workers: int = 0,
     out_norm: str = "min_max",
-    fuel_feats_processing: str = "ordinal",
-    fuel_feats_ordinal_is_norm: bool | None = True,
+    fuel_feats_encoding: str = "ordinal",
+    normalize_fuel_feats_ordinal: bool | None = True,
     modelling_approach: str = "2",
     transform: Callable | None = None,
     feature_names_list: list[str] | None = None,
@@ -235,8 +239,8 @@ def get_test_loader(
         root_dir=root_dir,
         filename_col=filename_col,
         out_norm=out_norm,
-        fuel_feats_processing=fuel_feats_processing,
-        fuel_feats_ordinal_is_norm=fuel_feats_ordinal_is_norm,
+        fuel_feats_encoding=fuel_feats_encoding,
+        normalize_fuel_feats_ordinal=normalize_fuel_feats_ordinal,
         modelling_approach=modelling_approach,
         transform=transform,
         feature_names_list=feature_names_list,
@@ -256,8 +260,8 @@ if __name__ == "__main__":
         val_csv_path="../yan_bp3/data_samples_approach_2/val_indices.csv",
         root_dir="../yan_bp3",
         out_norm=out_norm,
-        fuel_feats_processing="one_hot",
-        fuel_feats_ordinal_is_norm=True,
+        fuel_feats_encoding="one_hot",
+        normalize_fuel_feats_ordinal=True,
         modelling_approach=modelling_approach,
         batch_size=4,
         transform=None,
@@ -269,8 +273,8 @@ if __name__ == "__main__":
         root_dir="../yan_bp3",
         batch_size=4,
         out_norm=out_norm,
-        fuel_feats_processing="one_hot",
-        fuel_feats_ordinal_is_norm=True,
+        fuel_feats_encoding="one_hot",
+        normalize_fuel_feats_ordinal=True,
         modelling_approach=modelling_approach,
         transform=None,
         feature_names_list=feature_names_list,
