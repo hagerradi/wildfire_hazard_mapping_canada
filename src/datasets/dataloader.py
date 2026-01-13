2775b091dfa8ed401d8abab3dde8258b80b73b4b
hagerradi <hager.radi@mila.quebec>
2026-01-13 20:58:50 +0200

    bug fix

--- src/datasets/dataloader.py
+++ src/datasets/dataloader.py
@@ -110,6 +110,7 @@ class GridDataset(Dataset):
         self.modelling_approach = modelling_approach
         self.root_dir = root_dir
         self.valid_mask_threshold = valid_mask_threshold
+        self.feature_names_list = feature_names_list
 
         self.metadata_df = pd.read_csv(os.path.join(self.root_dir, csv_name))
         self.metadata_df = self.metadata_df[self.metadata_df["valid_ratio"] > self.valid_mask_threshold]
@@ -130,14 +131,13 @@ class GridDataset(Dataset):
             )  # For modelling approach 2: total number of unique interations that produced fires for a single season and cause
 
         self.channel_indices = None
-        with open(os.path.join(root_dir, f"feature_channel_map_{self.modelling_approach}.json")) as f:
-            channel_feature_map = json.load(f)
-        self.fuel_feat_index = channel_feature_map["fuel_grid"][0]
-        if feature_names_list:
+
+        if self.feature_names_list:
             with open(os.path.join(self.root_dir, f"feature_channel_map_{self.modelling_approach}.json")) as f:
                 channel_feature_map = json.load(f)
-                self.channel_indices = [item for key in feature_names_list for item in channel_feature_map[key]]
-            self.fuel_feat_index = self.channel_indices.index(self.fuel_feat_index)
+                self.channel_indices = [item for key in self.feature_names_list for item in channel_feature_map[key]]
+                if "fuel_grid" in self.feature_names_list:
+                    self.fuel_feat_index = channel_feature_map["fuel_grid"][0]
 
     def __len__(self):
         return len(self.metadata_df)
@@ -151,6 +151,7 @@ class GridDataset(Dataset):
 
         data = np.load(file_path).astype(np.float32)
         input_arr, output_arr = data[:, :, :-1], data[:, :, -1]
+
         input_arr = input_arr[:, :, self.channel_indices] if self.channel_indices else input_arr
         output_arr[np.isnan(output_arr)] = 0.0
 
@@ -158,14 +159,14 @@ class GridDataset(Dataset):
         mask = ~np.isnan(input_arr[:, :, 0])  # mask is True where not NaN, False where NaN
 
         # Processing one hot encoding
-        if self.fuel_feats_encoding == "one_hot":  # (H,W,C+20)
+        if "fuel_grid" in self.feature_names_list and self.fuel_feats_encoding == "one_hot":  # (H,W,C+20)
             input_arr = one_hot_encode(arr=input_arr, channel_idx=self.fuel_feat_index, num_classes=int(MAX_FUEL_GRID + 1))
 
         input_arr = fill_nan_channel_mean_numpy(input_arr)  # remove NaNs from the inp data (replace by mean)
 
         # Processing ordinal encoding norm (if not norm do nothing)
-        if self.fuel_feats_encoding == "ordinal":
-            input_arr[:, :, self.fuel_feat_index][mask] = 0.0  # Nan is no fuel
+        if "fuel_grid" in self.feature_names_list and self.fuel_feats_encoding == "ordinal":
+            input_arr[:, :, self.fuel_feat_index][~mask] = 0.0  # Nan is no fuel
             if self.normalize_fuel_feats_ordinal:
                 input_arr[:, :, self.fuel_feat_index] = (input_arr[:, :, self.fuel_feat_index] - MIN_FUEL_GRID) / (
                     MAX_FUEL_GRID - MIN_FUEL_GRID
