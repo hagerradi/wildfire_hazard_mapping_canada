72a39f46c0a8fc0d509db1ba79632f46c9f59560
hagerradi <hager.radi@mila.quebec>
2026-01-22 23:40:16 +0200

    code review 2

--- run_files/train.sh
+++ run_files/train.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+##SBATCH --mail-type=all
+##SBATCH --mail-user=name@mila.quebec
 #SBATCH --job-name=unet_full_data
 #SBATCH --output=logs/job_%x_%j.out
 #SBATCH --error=logs/job_%x_%j.err
+#SBATCH --partition=long
 #SBATCH --ntasks=1
 #SBATCH --time=05:59:00
 #SBATCH --mem-per-cpu=10Gb
