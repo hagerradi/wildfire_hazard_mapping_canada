0f0c3ce6a6e49c6b8e2e1a85b9d37885312c445c
Chirag Goel <chirag.goel@mila.quebec>
2026-03-09 17:22:55 +0530

    added code for hexel splitting for using the center crop

--- data_preparation/process_hexels_into_grids.py
+++ data_preparation/process_hexels_into_grids.py
@@ -8,7 +8,7 @@ import pandas as pd
 from data_preparation.generate_season_cause_output import FireCountRasterizer
 from data_preparation.grid_loader.utils import NODATA, load_fire_shapefiles
 from data_preparation.hexel_loader import load_features_per_hexel
-from data_preparation.utils import HEX_ID_NA, find_hex_ids, get_processed_hex_ids
+from data_preparation.utils import HEX_ID_NA, find_hex_ids, get_padding_params, get_processed_hex_ids
 
 
 def save_split_hexel_windows(
@@ -44,18 +44,19 @@ def get_split_hexel_window(
     rasterizer = FireCountRasterizer(shp_paths, None)
     total_unique_iters = rasterizer.get_num_unique_iters(season=None, cause=None)
     num_season_cause, H, W, _ = season_cause_stacked_feats.shape
-    stride_h = max(1, int(win_h * (1 - overlap_ratio)))  # n_rows = (H-win_h)//stride_h + 1
-    stride_w = max(1, int(win_w * (1 - overlap_ratio)))
+    pad_top, pad_bot, pad_left, pad_right, stride_h, stride_w = get_padding_params(H, W, win_h, win_w, overlap_ratio)
 
-    # Adding padding for the edges
-    pad_h = stride_h - (H - win_h) % stride_h if (H - win_h) % stride_h != 0 else 0
-    pad_w = stride_w - (W - win_w) % stride_w if (W - win_w) % stride_w != 0 else 0
+    # Apply the calculated padding
     season_cause_stacked_feats_padded = np.pad(
-        season_cause_stacked_feats, ((0, 0), (0, pad_h), (0, pad_w), (0, 0)), mode="constant", constant_values=NODATA
+        season_cause_stacked_feats, ((0, 0), (pad_top, pad_bot), (pad_left, pad_right), (0, 0)), mode="constant", constant_values=NODATA
     )
     season_cause_mask_padded = np.pad(
-        season_cause_mask, ((0, 0), (0, pad_h), (0, pad_w)), mode="constant", constant_values=1.0
-    )  # Mask should be 1 where nan
+        season_cause_mask,
+        ((0, 0), (pad_top, pad_bot), (pad_left, pad_right)),
+        mode="constant",
+        constant_values=1.0,  # Mask should be 1 where nan
+    )
+
     # season_cause_stacked_feats_padded[:,:,:,-1][season_cause_mask_padded] = 0.0 #For outputs, mask means 0 probability
     _, H_pad, W_pad, _ = season_cause_stacked_feats_padded.shape
 
@@ -137,7 +138,7 @@ def generate_data_samples(
 
     if is_array_job:
         # 1. Get all Hex IDs
-        hex_ids = find_hex_ids(root_dir)
+        hex_ids = ["41"]  # find_hex_ids(root_dir)
         # Sort them to ensure every worker sees the same order
         hex_ids = sorted(list(hex_ids))
 
@@ -148,7 +149,7 @@ def generate_data_samples(
             print(f"[Worker {task_id}/{num_tasks}] Processing {len(my_hexels)} hexels out of {len(hex_ids)} total.")
             hex_ids = my_hexels
     else:
-        hex_ids = find_hex_ids(root_dir)
+        hex_ids = ["41"]  # find_hex_ids(root_dir)
 
     for hex_id in hex_ids:
         if hex_id in completed_hex_ids:
