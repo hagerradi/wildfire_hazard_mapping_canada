4df95fdb98d646ce1aa5174d3b9b3dd1c42df4c1
chirag <chirag.goel@mila.quebec>
2026-03-09 10:05:55 -0400

    auxilary name change

--- src/config.py
+++ src/config.py
@@ -25,10 +25,10 @@ class ModelConfig(BaseModel):
     use_transpose_conv: bool = False
     use_activation_after_upsampling: bool = False
 
-    # specific to tabular model
-    auxillary_hidden_dims: dict[str, list[int] | dict[str, list[int]]] = {"weather": [32, 64]}
-    auxillary_embed_dims: dict[str, int] = {"weather": 128}
-    auxillary_poolings: dict[str, str] = {"weather": "max"}
+    # specific to auxilary model
+    auxilary_hidden_dims: dict[str, list[int] | dict[str, list[int]]] = {"weather": [32, 64]}
+    auxilary_embed_dims: dict[str, int] = {"weather": 128}
+    auxilary_poolings: dict[str, str] = {"weather": "max"}
 
 
 class OptimizerConfig(BaseModel):
