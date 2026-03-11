83c572759d4d0f10c54f785c6a551ba09845ec01
alzaia <aldo.zaimi+1@mila.quebec>
2026-03-11 11:50:43 -0400

    colormap

--- src/datasets/postprocessing/visualize_predictions.py
+++ src/datasets/postprocessing/visualize_predictions.py
@@ -169,8 +169,8 @@ def plot_hexbin_distribution(
     else:
         max_limit = 0.15
 
-    fig, ax = plt.subplots(figsize=(8, 7))
-    hb = ax.hexbin(gt_vals, pred_vals, gridsize=100, cmap="viridis", bins="log", mincnt=1)
+    fig, ax = plt.subplots(figsize=(8, 6))
+    hb = ax.hexbin(gt_vals, pred_vals, gridsize=100, cmap="inferno", bins="log", mincnt=1)
 
     ax.plot([0, max_limit], [0, max_limit], color="black", linestyle="--", linewidth=2, label="Perfect Alignment")
 
