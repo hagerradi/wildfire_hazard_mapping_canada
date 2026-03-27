f678d6b024ebc1ee3b392d8e0e54d7b5ce5452db
chirag <chirag.goel@mila.quebec>
2026-03-27 14:42:02 -0400

    added wind_spatial and wind_mixer to possible data sources

--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from data_preparation.grid_loader.utils import fuel_ranking
 
-AVAILABLE_DATA_SOURCES = ["grid", "weather", "fire_size", "wind_grid"]
+AVAILABLE_DATA_SOURCES = ["grid", "weather", "fire_size", "wind_grid_mixer", "wind_grid_spatial"]
 MAX_FUEL_GRID = float(max(fuel_ranking.values()))
 
 
@@ -10,7 +10,7 @@ def get_data_source_class(name: str):
     """
     Returns the source class dynamically to avoid circular imports.
     """
-    if name in ["grid", "wind_grid"]:
+    if name in ["grid", "wind_grid_mixer", "wind_grid_spatial"]:
         from src.datasets.sources import GridSource
 
         return GridSource
